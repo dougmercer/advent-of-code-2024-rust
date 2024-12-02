24128b29db418fcdcc7b16892977f6e0593087ea
Doug Mercer <dougmerceryt@gmail.com>
2024-12-02 08:55:02 -0500

    Day2 Part2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -17,26 +17,46 @@ fn read_input(path: &str) -> Result<Vec<Vec<i32>>, Box<dyn Error>> {
     Ok(reports)
 }
 
-fn part1(path: &str) -> Result<i32, Box<dyn Error>> {
-    let reports = read_input(path)?;
+fn is_safe(report: &[i32]) -> bool {
+    let monotonic: bool =
+        report.windows(2).all(|w| w[0] <= w[1]) || report.windows(2).all(|w| w[0] >= w[1]);
+    let valid_diffs = report.windows(2).all(|w| {
+        let diff = (w[0] - w[1]).abs();
+        diff >= 1 && diff <= 3
+    });
+    monotonic && valid_diffs
+}
 
-    fn is_safe(report: &Vec<i32>) -> bool {
-        let monotonic: bool =
-            report.windows(2).all(|w| w[0] <= w[1]) || report.windows(2).all(|w| w[0] >= w[1]);
-        let valid_diffs = report.windows(2).all(|w| {
-            let diff = (w[0] - w[1]).abs();
-            diff >= 1 && diff <= 3
-        });
-        monotonic && valid_diffs
+fn hold_out(x: &[i32], i: usize) -> Vec<i32> {
+    x.iter()
+    .take(i)
+    .chain(x.iter().skip(i+1))
+    .copied()
+    .collect()
+}
+
+fn problem_dampener(report: &[i32]) -> bool {
+    let mut _is_safe = false;
+    for i in 0..report.len() {
+        let report_without_i = hold_out(report, i);
+        _is_safe = _is_safe || is_safe(&report_without_i);
     }
+    _is_safe
+}
 
-    Ok(reports.iter().filter(|&report| is_safe(report)).count() as i32)
+fn part1(path: &str) -> Result<usize, Box<dyn Error>> {
+    let reports: Vec<Vec<i32>> = read_input(path)?;
+    Ok(reports.iter().filter(|&report| is_safe(report)).count())
+}
+
+fn part2(path: &str) -> Result<usize, Box<dyn Error>> {
+    let reports: Vec<Vec<i32>> = read_input(path)?;
+    Ok(reports.iter().filter(|&report| problem_dampener(report)).count())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // let path: &str = "data/day2.sample.csv";
     let path: &str = "data/day2.input";
     println!("Part 1: {:?}", part1(path)?);
-    // println!("Part 2: {:?}", part2(path)?);
+    println!("Part 2: {:?}", part2(path)?);
     Ok(())
 }
