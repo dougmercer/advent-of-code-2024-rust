c76f068ff00a6b3845e1c1bc4de70a4acf8932a6
Doug Mercer <dougmerceryt@gmail.com>
2024-12-06 16:09:38 -0500

    Massively refactor and finish part 2 of day 3

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,83 +1,107 @@
-// Didn't really have time to do today's challenge cause I was on travel =/
+use atoi::atoi;
 use std::{error::Error, fs};
 
 fn read_input(path: &str) -> Result<String, Box<dyn Error>> {
     Ok(fs::read_to_string(path)?)
 }
 
-fn part1(path: &str) -> Result<usize, Box<dyn Error>> {
-    let code = read_input(path)?;
-    let chars: Vec<char> = code.chars().collect();
-    let mut i: usize = 0;
-    let mut j: usize;
-    let mut a: i32;
-    let mut b: i32;
-    let mut answer: i32 = 0;
-    while i < chars.len() {
-        if chars[i] != 'm' {
-            i += 1;
-            continue;
-        }
-        i += 1;
-        if chars[i] != 'u' {
-            i += 1;
-            continue;
-        }
-        i += 1;
-        if chars[i] != 'l' {
-            i += 1;
-            continue;
-        }
-        i += 1;
-        if chars[i] != '(' {
-            i += 1;
-            continue;
-        }
-        i += 1;
-        j = 0;
-        while (i + j) < chars.len() && chars[i + j].is_digit(10) {
-            j += 1
-        }
-        if j > 3 {
-            i += j;
-            continue;
-        } else {
-            let num_str_a: String = chars[i..i + j].iter().collect();
-            a = num_str_a.parse::<i32>().unwrap();
-        }
-        i += j;
+enum Command {
+    Mul(i32),
+    Do,
+    Dont,
+}
 
-        if chars[i] != ',' {
-            i += 1;
-            continue;
-        }
-        i += 1;
-        j = 0;
-        while (i + j) < chars.len() && chars[i + j].is_digit(10) {
-            j += 1
-        }
-        if j > 3 {
-            i += j;
-            continue;
-        } else {
-            let num_str_b: String = chars[i..i + j].iter().collect();
-            b = num_str_b.parse::<i32>().unwrap();
-        }
-        i += j;
-        if chars[i] != ')' {
-            i += 1;
-            continue;
-        }
-        i += 1;
-        // println!("a={} b={}", a, b);
-        answer += a * b;
+// Parse a number of up to 3 digits, returning (number, chars consumed)
+fn parse_number(bytes: &[u8]) -> Option<(i32, usize)> {
+    let len = bytes
+        .iter()
+        .take_while(|&&b| b.is_ascii_digit())
+        .take(3)
+        .count();
+
+    if len == 0 {
+        return None;
     }
 
-    Ok(answer.try_into().unwrap())
+    Some((atoi(&bytes[..len])?, len))
+}
+
+fn try_parse_mul(bytes: &[u8]) -> Option<(Command, usize)> {
+    if bytes.starts_with(b"mul(") {
+        let (a, consumed) = parse_number(&bytes[4..])?;
+        let next_pos = 4 + consumed;
+
+        if bytes.get(next_pos) != Some(&b',') {
+            return None;
+        }
+
+        let (b, consumed) = parse_number(&bytes[next_pos + 1..])?;
+        let next_pos = next_pos + 1 + consumed;
+
+        if bytes.get(next_pos) != Some(&b')') {
+            return None;
+        }
+
+        return Some((Command::Mul(a * b), next_pos + 1));
+    }
+
+    None
+}
+
+fn try_parse_command(bytes: &[u8]) -> Option<(Command, usize)> {
+    // Try to parse "do()"
+    if bytes.starts_with(b"do()") {
+        return Some((Command::Do, 4));
+    }
+
+    // Try to parse "don't()"
+    if bytes.starts_with(b"don't()") {
+        return Some((Command::Dont, 7));
+    }
+
+    try_parse_mul(bytes)
+}
+
+fn part1(path: &str) -> Result<usize, Box<dyn Error>> {
+    let content = read_input(path)?;
+    let bytes = content.as_bytes();
+
+    let sum: i32 = (0..bytes.len())
+        .filter_map(|i| try_parse_command(&bytes[i..]))
+        .fold(0, |sum, (command, _)| match command {
+            Command::Mul(result) => sum + result,
+            _ => sum,
+        });
+
+    Ok(sum.try_into().unwrap())
+}
+
+fn part2(path: &str) -> Result<usize, Box<dyn Error>> {
+    let content = read_input(path)?;
+    let bytes = content.as_bytes();
+
+    let mut enabled = true;
+    let sum: i32 = (0..bytes.len())
+        .filter_map(|i| try_parse_command(&bytes[i..]))
+        .fold(0, |sum, (command, _)| match command {
+            Command::Mul(result) if enabled => sum + result,
+            Command::Do => {
+                enabled = true;
+                sum
+            }
+            Command::Dont => {
+                enabled = false;
+                sum
+            }
+            _ => sum,
+        });
+
+    Ok(sum.try_into().unwrap())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let path: &str = "data/day3.input";
     println!("Part 1: {:?}", part1(path)?);
+    println!("Part 2: {:?}", part2(path)?);
     Ok(())
 }
